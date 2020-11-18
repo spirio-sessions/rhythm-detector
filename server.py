5eed3c1514c55142eca0d6f55ead71f3fe962c64
Ismael Agchar <ismael.agchar@gmail.com>
2020-11-18 02:36:56 +0100

    changed basis of server

--- server.py
+++ server.py
@@ -1,25 +1,39 @@
-__server_name__ = '' 
-__server_port__ = 8080
+class Server:
 
-from http.server import HTTPServer, BaseHTTPRequestHandler
+    def __init__(self, name, port):
+        self.name = name
+        self.port = port
+        self.handle = lambda _: None
 
-class HelloWorld(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write('hello from xmaek!'.encode('utf-8'))
+    def with_handle(self, callback):
+        self.handle = callback
+        return self
 
-def run(server_address, handler):
-    httpd = HTTPServer(server_address, handler)
-    print('starting server on %s' % str(server_address))
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
+    def run(self):
         pass
-    print('stopping server')
 
-if __name__ == '__main__':
-    server_address = (__server_name__, __server_port__)
-    handler = HelloWorld
-    run(server_address, handler)
+# __server_name__ = '' 
+# __server_port__ = 8080
+
+# from http.server import HTTPServer, BaseHTTPRequestHandler
+
+# class HelloWorld(BaseHTTPRequestHandler):
+#     def do_GET(self):
+#         self.send_response(200)
+#         self.send_header('Content-type', 'text/html')
+#         self.end_headers()
+#         self.wfile.write('hello from xmaek!'.encode('utf-8'))
+
+# def run(server_address, handler):
+#     httpd = HTTPServer(server_address, handler)
+#     print('starting server on %s' % str(server_address))
+#     try:
+#         httpd.serve_forever()
+#     except KeyboardInterrupt:
+#         pass
+#     print('stopping server')
+
+# if __name__ == '__main__':
+#     server_address = (__server_name__, __server_port__)
+#     handler = HelloWorld
+#     run(server_address, handler)
