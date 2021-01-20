acaee33dd05d5d036d2edc51c5a4e9a861ba4416
Ismael Agchar <ismael.agchar@gmail.com>
2021-01-20 13:22:22 +0100

    added analyser profile for sustained play

--- analyser_test.py
+++ analyser_test.py
@@ -4,17 +4,22 @@ from matplotlib import pyplot
 from loader import Loader
 from analyser import Analyser
 
-loader = Loader('/Users/xmaek/Music/Music/Media.localized/Unknown Artist/Unknown Album/Sax_1.wav', 0)
-signal = loader.load(chunk_length=5.0)
+chunk_length = 10.0
+loader = Loader('/Users/xmaek/Music/Music/Media.localized/Unknown Artist/Unknown Album/Sax_2.wav', 0)
+signal = loader.load(chunk_length=chunk_length)
 # loader = Loader('./recording.wav', 0)
-# signal = loader.load(chunk_length=2.0)
+# signal = loader.load(chunk_length=chunk_length)
 
 # timestamps_raw = [ i/loader.get_sample_rate() for i in range(len(signal)) ]
 # pyplot.figure(figsize=(20,5))
 # pyplot.plot(timestamps_raw, signal, '.')
 # pyplot.show()
 
-analyser = Analyser(sample_rate=loader.get_sample_rate(), chunk_length=2.0)
+analyser = Analyser(
+    sample_rate=loader.get_sample_rate(),
+    chunk_length=chunk_length,
+    window_length=0.4,
+    dominant_threshold=10000000)
 
 smoothed_signal = analyser.smooth(signal)
 detection_signal = analyser.detect(smoothed_signal)
@@ -25,11 +30,11 @@ timestamps = [ i*analyser.hop_length for i in range(len(smoothed_signal))]
 pyplot.figure(figsize=(20,5))
 pyplot.plot(timestamps, smoothed_signal)
 pyplot.ylim(2.5e8, 6.0e8)
-# pyplot.show()
+pyplot.show()
 
-# pyplot.figure(figsize=(20,5))
-# pyplot.plot(timestamps, detection_signal)
-# pyplot.ylim(-0.5e8, 0.5e8)
+pyplot.figure(figsize=(20,5))
+pyplot.plot(timestamps, detection_signal)
+pyplot.ylim(-0.5e8, 0.5e8)
 # pyplot.show()
 
 # pyplot.figure(figsize=(20,5))
@@ -37,19 +42,8 @@ pyplot.ylim(2.5e8, 6.0e8)
 # pyplot.ylim(0.5, 1.5)
 # pyplot.show()
 
-beats = analyser.peaks_to_timestamps(peak_signal)
+beats = analyser.analyse(signal)
 for timestamp, _ in beats:
     pyplot.axvline(x=timestamp, color='r')
 pyplot.show()
-
-# %%
-from loader import Loader
-from analyser import Analyser
-
-loader = Loader('/Users/xmaek/Music/Music/Media.localized/Unknown Artist/Unknown Album/Sax_1.wav', 0)
-signal = loader.load(chunk_length=2.5)
-
-analyser = Analyser(sample_rate=loader.get_sample_rate(), chunk_length=2.5)
-dominants = analyser.analyse(signal)
-print(dominants)
 # %%
